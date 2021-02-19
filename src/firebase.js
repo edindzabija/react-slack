813b6e72ceb881fb0464ef3efd8b3aa4f807a51f
Edin Dzabija <edindzabija@gmail.com>
2021-02-19 23:40:18 +0100

    firebase, react router

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB43y4bqxRaibksCA30X7Sub-iB2kD0EPc',
+  authDomain: 'react-slack-5ead0.firebaseapp.com',
+  projectId: 'react-slack-5ead0',
+  storageBucket: 'react-slack-5ead0.appspot.com',
+  messagingSenderId: '371450743247',
+  appId: '1:371450743247:web:49f985cb4cd2b0a00beaba',
+  measurementId: 'G-ER5G8PQER4',
+}

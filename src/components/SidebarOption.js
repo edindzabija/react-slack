690e043d134b72e94eac2d595ba74ea4e61cf565
Edin Dzabija <edindzabija@gmail.com>
2021-02-23 01:31:00 +0100

    redux, add/switch channels

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
+import { enterRoom } from '../features/appSlice'
 import { db } from '../firebase'
 
 const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
+  const dispatch = useDispatch()
 
   const addChannel = () => {
     const channelName = prompt('Please enter the channel name')
@@ -15,7 +18,13 @@ const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
   }
 
   const selectChannel = () => {
-    
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      )
+    }
   }
 
   return (

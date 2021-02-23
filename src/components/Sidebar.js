6c97f66da03f81fbac08c2a954741f8cd3ae47aa
Edin Dzabija <edindzabija@gmail.com>
2021-02-23 01:13:24 +0100

    sidebar, add channel

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { db } from '../firebase'
+
+import SidebarOption from './SidebarOption'
 
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
 import CreateIcon from '@material-ui/icons/Create'
+import InsertCommentIcon from '@material-ui/icons/InsertComment'
+import InboxIcon from '@material-ui/icons/Inbox'
+import DraftsIcon from '@material-ui/icons/Drafts'
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt'
+import AppsIcon from '@material-ui/icons/Apps'
+import FileCopyIcon from '@material-ui/icons/FileCopy'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import AddIcon from '@material-ui/icons/Add'
 
 const Sidebar = () => {
+  const [channels, loading, error] = useCollection(db.collection('rooms'))
+
   return (
     <SidebarContainer>
       <SidebarHeader>
@@ -18,6 +34,21 @@ const Sidebar = () => {
         <CreateIcon />
       </SidebarHeader>
 
+      <SidebarOption Icon={InsertCommentIcon} title='Threads' />
+      <SidebarOption Icon={InboxIcon} title='Mentions & reactions' />
+      <SidebarOption Icon={DraftsIcon} title='Saved items' />
+      <SidebarOption Icon={BookmarkBorderIcon} title='Channel browser' />
+      <SidebarOption Icon={PeopleAltIcon} title='People & user groups' />
+      <SidebarOption Icon={AppsIcon} title='Apps' />
+      <SidebarOption Icon={FileCopyIcon} title='File browser' />
+      <SidebarOption Icon={ExpandLessIcon} title='Show less' />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title='Channels' />
+      <hr />
+      <SidebarOption Icon={AddIcon} title='Add Channel' addChannelOption />
+      {channels?.docs.map((doc) => (
+        <SidebarOption title={doc.data().name} key={doc.id} id={doc.id} />
+      ))}
     </SidebarContainer>
   )
 }
@@ -31,6 +62,12 @@ const SidebarContainer = styled.div`
   border-top: 1px solid #49274b;
   max-width: 260px;
   margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `
 
 const SidebarHeader = styled.div`
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: 'AIzaSyB43y4bqxRaibksCA30X7Sub-iB2kD0EPc',
   authDomain: 'react-slack-5ead0.firebaseapp.com',
@@ -7,3 +9,10 @@ const firebaseConfig = {
   appId: '1:371450743247:web:49f985cb4cd2b0a00beaba',
   measurementId: 'G-ER5G8PQER4',
 }
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { auth, provider, db }
